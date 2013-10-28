f94f8fa02a021e5030a588ca9fe16d990a7c463b
Daniel Rune Jensen <danielrunejensen@gmail.com>
2013-10-28 17:41:07 +0100

    Add CameraController

---
+++ ThePrinterGuy/Assets/Scripts/CameraController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject printer;
+
+    OnStart(){
+
+
+}
+
+    void OnEnable()
+    {
+        GestureManager.OnSwipeRight += CameraRotation;
+
+    }
+
+    public void CameraRotation()
+    {
+        iTween.MoveTo(GameObject, "lookTarget", printer.transform.position, "position", new Vector3(-10,3,0));
+    }
+}
