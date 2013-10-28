f36230c3a0607db5dd7c8f0190405f3e40f4a338
Anders Jensen <ajen2112@gmail.com>
2013-10-28 16:05:54 +0100

    TimeUtility class

---
+++ ThePrinterGuy/Assets/Scripts/TimerUtilities.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TimerUtilities : MonoBehaviour
+{
+    [SerializeField]
+    private float _duration = 60;
+
+    private float _startTime;
+    private float _endTime;
+
+    private bool _isElapsed = false;
+
+    // Use this for initialization
+    void Start()
+    {
+        _startTime = Time.time;
+        _endTime = _startTime + _duration;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(_isElapsed == false)
+        {
+            if(Time.time >= _endTime)
+            {
+                _isElapsed = true;
+            }
+        }
+    }
+
+    public float getTimeLeft()
+    {
+        return Time.time - _endTime;
+    }
+
+    public void DestroyTimer()
+    {
+        Destroy(this);
+    }
+
+}
