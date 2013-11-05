01055e60f0076aaeac5c6eb979f644510360f8a2
Danny Tamsen <Danta1st@gmail.com>
2013-11-05 14:16:45 +0100

    Updated and cleaned up the paper jam functionality

--- ThePrinterGuy/Assets/Scripts/PaperJam.cs
+++ ThePrinterGuy/Assets/Scripts/PaperJam.cs
@@ -5,76 +5,68 @@ public class PaperJam : MonoBehaviour
 {
     #region Editor Publics
     [SerializeField]
-    private float _shakeJamInOut = 10.0f;
+    private int _jamChanceInPercent = 1;
     [SerializeField]
-    private float _shakeJamLeftRight = 3.0f;
-    [SerializeField]
-    private float _shakeJamRotate = 0.5f;
-    [SerializeField]
-    private float _shakeJamTime = 1.5f;
-    [SerializeField]
-    private float _shakeInOut = 2.0f;
-    [SerializeField]
-    private float _shakeLeftRight = 0.5f;
-    [SerializeField]
-    private float _shakeRotate = 0.0f;
-    [SerializeField]
-    private float _shakeTime = 0.5f;
-    [SerializeField]
-    private int _paperJamMaxRate = 100;
-    [SerializeField]
-    private int _paperJamChance = 1;
-    [SerializeField]
-    private float _litRotation = 0.0f;
-    [SerializeField]
-    private float _litRotationTime = 2.0f;
+    private float _openAndCloseTime = 1.0f;
+	[SerializeField]
+	private iTween.EaseType _openEaseType;
+	[SerializeField]
+	private iTween.EaseType _closeEaseType;
+	[SerializeField]
+	private GameObject _jammedItem;
     #endregion
 
     #region Privates
+    private float _shakeJamInOut = 10.0f;
+    private float _shakeJamLeftRight = 3.0f;
+    private float _shakeJamRotate = 0.5f;
+    private float _shakeJamTime = 1.5f;
+	
+    private float _shakeInOut = 2.0f;
+    private float _shakeLeftRight = 0.5f;
+    private float _shakeRotate = 0.0f;
+    private float _shakeTime = 0.5f;
+	
+    private int _paperJamMaxRate = 100;
+    private float _litRotation = -90.0f;
+	
     private Vector3 _shakeJam;
     private Vector3 _shakePrint;
     private Quaternion _startRotation;
-    private GameObject _smokePrefabHolder;
-    private GameObject _paperJam;
-    private GameObject _particleHolder;
     private GameObject _paperJamHolder;
     private bool _isJammed;
     private float _lidStartRotation;
     private bool _litOpen = false;
+	private ParticleSystem[] _smokes;
     #endregion
 
     #region Delegates
     public delegate void OnJamAction();
     public static event OnJamAction OnJam;
+	
     public delegate void OnUnjammedAction();
     public static event OnUnjammedAction OnUnjammed;
     #endregion
-
-    void Awake()
-    {
-        _smokePrefabHolder = Resources.Load("Effects/jamSmoke", typeof(GameObject)) as GameObject;
-
-        _paperJam = Resources.Load("Effects/paperJam", typeof(GameObject)) as GameObject;
-
-        _smokePrefabHolder.transform.position = transform.position;
-    }
-
+	
+	#region Monobehaviour Functions
     void Start()
     {
         _startRotation = gameObject.transform.rotation;
         _shakeJam = new Vector3(_shakeJamRotate, _shakeJamLeftRight, _shakeJamInOut);
         _shakePrint = new Vector3(_shakeRotate, _shakeLeftRight, _shakeInOut);
-
-        _smokePrefabHolder.particleSystem.playOnAwake = true;
-        _smokePrefabHolder.particleSystem.enableEmission = false;
-        _particleHolder = (GameObject)Instantiate(_smokePrefabHolder);
     }
+	
+    void Update()
+    {
+		if(Input.anyKey)
+        	OnPaperPrint(gameObject);
+    }
+	#endregion
 
     #region Enable/Disable
     void OnEnable()
     {
         OnJam += Shake;
-        OnJam += Smoke;
         OnUnjammed += JamStopped;
         PrinterManager.OnPagePrinted += OnPaperPrint;
         GestureManager.OnTap += ResolvePaperJam;
@@ -83,7 +75,6 @@ public class PaperJam : MonoBehaviour
     void OnDisable()
     {
         OnJam -= Shake;
-        OnJam -= Smoke;
         OnUnjammed -= JamStopped;
         PrinterManager.OnPagePrinted -= OnPaperPrint;
         GestureManager.OnTap -= ResolvePaperJam;
@@ -96,11 +87,6 @@ public class PaperJam : MonoBehaviour
         iTween.PunchRotation(gameObject, iTween.Hash("amount", _shakeJam, "time", _shakeJamTime, "oncomplete", "Shake"));
     }
 
-    void Smoke()
-    {
-        _particleHolder.particleSystem.enableEmission = true;
-    }
-
     void JamStopped()
     {
         iTween.Stop(gameObject);
@@ -109,35 +95,30 @@ public class PaperJam : MonoBehaviour
 
     #region PaperJam Occurs is Resolved
     void OnPaperPrint(GameObject printObject)
-    {
+    {		
         iTween.PunchRotation(gameObject, iTween.Hash("amount", _shakePrint, "time", _shakeTime));
 
-        if(Random.Range(0, _paperJamMaxRate) <= _paperJamChance)
+        if(Random.Range(0, _paperJamMaxRate) <= _jamChanceInPercent)
         {
             if(OnJam != null)
+			{
                 _isJammed = true;
                 OnJam();
-                Vector3 tmpPos = transform.position;
-                tmpPos.y += 2;
-                tmpPos.z += 1;
-                _paperJam.transform.position = tmpPos;
+                Vector3 tmpPos = new Vector3(transform.position.x, transform.position.y+2, transform.position.z+1);
+			
+                _jammedItem.transform.position = tmpPos;
+                _jammedItem.transform.localScale = new Vector3(1.5f, 0.15f, 1.5f);
 
-                //preplace with paper model
-                _paperJam.transform.localScale = new Vector3(1.5f, 0.15f, 1.5f);
-
-                _paperJamHolder = (GameObject)Instantiate(_paperJam);
+                _paperJamHolder = (GameObject)Instantiate(_jammedItem);
+			}
         }
-    }
-
-    void Update()
-    {
-        OnPaperPrint(gameObject);
     }
 
     void ResolvePaperJam(GameObject thisGameObj, Vector2 screenPos)
     {
         if(thisGameObj != null)
         {
+			//Open or Close the lid
             if(thisGameObj.tag == "JamTask" && !_litOpen && _isJammed)
             {
                 Debug.Log("LIT HIT");
@@ -146,25 +127,25 @@ public class PaperJam : MonoBehaviour
                 transform.rotation = _startRotation;
 
                 _lidStartRotation = thisGameObj.transform.rotation.x;
-                iTween.RotateAdd(thisGameObj, new Vector3(_litRotation, 0, 0), _litRotationTime);
+                iTween.RotateAdd(thisGameObj, iTween.Hash("amount", new Vector3(_litRotation, 0, 0), "time", _openAndCloseTime, "easetype", _openEaseType));
                 _litOpen = true;
             }
             else if(thisGameObj.tag == "JamTask" && _litOpen && !_isJammed)
             {
                 iTween.Stop(gameObject);
     
-                iTween.RotateAdd(thisGameObj, new Vector3(-_litRotation, 0, 0), _litRotationTime);
+                iTween.RotateAdd(thisGameObj, new Vector3(-_litRotation, 0, 0), _openAndCloseTime);
                 _litOpen = false;
             }
-    
+    		
+			//Delete Jammed Item
             if(thisGameObj != null && _paperJamHolder != null)
             {
-                if(thisGameObj.tag == _paperJamHolder.tag)
+                if(thisGameObj.name == _paperJamHolder.name)
                 {
                     _isJammed = false;
                     OnUnjammed();
                     Destroy(thisGameObj);
-                    _particleHolder.particleSystem.enableEmission = false;
                 }
             }
         }
