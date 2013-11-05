5793a16a7e8c3d32e1a7162d428f1d7365aff584
Martin Leth Pedersen <mped10@student.aau.dk>
2013-11-05 10:10:27 +0100

    Add BoostButton Functionality
    Event for BoostOn, BoostOff - Used in highscore script.
    Needs to animate lever of printer, and maybe add shiny effects

---
+++ ThePrinterGuy/Assets/Scripts/BoostButton.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoostButton : MonoBehaviour {
+
+    #region Editor Publics
+    [SerializeField]
+    private float leverUnlockScore;
+    [SerializeField]
+    private float boostDuration = 5.0f;
+    [SerializeField]
+    private int boostCharges = 1;
+    #endregion
+
+    #region Privates
+    private float highscore;
+    #endregion
+
+    #region Delegates
+    public delegate void BoostOn();
+    public static event BoostOn BoostActivated;
+    public delegate void BoostOff();
+    public static event BoostOff BoostDeactivated;
+    #endregion
+
+	// Use this for initialization
+	void Start()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update()
+    {
+	
+	}
+
+    void OnEnable()
+    {
+        //GestureManager.OnDrag += OnLeverPull;
+        GestureManager.OnTap += OnLeverPull;
+    }
+
+    void OnDisable()
+    {
+        //GestureManager.OnDrag -= OnLeverPull;
+        GestureManager.OnTap -= OnLeverPull;
+    }
+
+    void Highscore(float newHighscore)
+    {
+        highscore = newHighscore;
+    }
+
+    void OnLeverPull(GameObject thisLever, Vector2 screenPos)//, Vector2 deltaPos)
+    {
+        if(gameObject == thisLever && highscore >= leverUnlockScore && boostCharges > 0)
+        {
+            boostCharges--;
+
+            if(BoostActivated != null)
+            {
+                BoostActivated();
+            }
+
+            Debug.Log("Boost Begun");
+
+            StartCoroutine(WaitForBoost());
+        }
+    }
+
+    IEnumerator WaitForBoost()
+    {
+        yield return new WaitForSeconds(boostDuration);
+
+        if(BoostDeactivated != null)
+        {
+            BoostDeactivated();
+        }
+
+        Debug.Log("Boost Over");
+    }
+}
