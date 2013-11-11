a541b95fbed2d00e5974ef06ad7aeaaea3e7d19a
Anders Jensen <ajen2112@gmail.com>
2013-11-11 13:22:32 +0100

    fix again

--- ThePrinterGuy/Assets/Scripts/InkController.cs
+++ ThePrinterGuy/Assets/Scripts/InkController.cs
@@ -131,6 +131,7 @@ public class InkController : MonoBehaviour
 		{
 			i.DisableCollider();
 			i.DisableRenderer();
+			i.gameObject.transform.position = i.GetStartPos();
 		}
 		
 		foreach(InkLid l in _inkLids)
