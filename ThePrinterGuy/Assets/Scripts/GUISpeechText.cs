2e3077e17d2293a58cd5d3ae7db10f68bf5a755a
Kasper Jensen Andersen <kjan09@student.aau.dk>
2013-11-14 16:19:32 +0100

    Speech bubbles: letter writer fixed

--- ThePrinterGuy/Assets/Scripts/GUISpeechText.cs
+++ ThePrinterGuy/Assets/Scripts/GUISpeechText.cs
@@ -3,69 +3,43 @@ using System.Collections;
 
 public class GUISpeechText : MonoBehaviour 
 {
-	
+	private TextMesh _speechTextMesh;
 	private int _currentPosition = 0;
 	private float _delay = 0.01f;
-	private string _text = "";
-	[SerializeField]
-	private string[] _additionalLines;
-	private float _scaleMultiplierY;
-	private float _fontSize = 300f;
-	
+	private string _outputText = "";
+	private string _inputText = "";
 
 	// Use this for initialization
 	void Start () {
-		
-		//_scaleMultiplierY = Screen.height / 1200f;
-		
-		StartCoroutine("LetterWriter");
-		
+		_speechTextMesh = GetComponent<TextMesh>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
-		if(Input.GetKeyDown(KeyCode.M))
-		{
-			// Writes "SpeechBubbleExample from the LocalizationText.xml
-			WriteText("SpeechBubbleExample");
-			
-		}
 	}
 	
 	public void WriteText(string _str)
 	{
-		TextMesh _test = GameObject.Find("SpeechText").GetComponent<TextMesh>();
-		
-		_test.text = LocalizationText.GetText(_str);
-		
-		string _aText = _test.text;
-		
-		//GetComponent<TextMesh>().fontSize = Mathf.CeilToInt(_fontSize * _scaleMultiplierY);
-		
-		GetComponent<TextMesh>().text = "";
+		_inputText = LocalizationText.GetText(_str);
+		_outputText = "";
+		GetComponent<TextMesh>().text = _outputText;
 		_currentPosition = 0;
-		_text = _aText;
-		
+
+		StartCoroutine("LetterWriter");
 	}
 	
 	IEnumerator LetterWriter()
 	{
-		
-		foreach(string _s in _additionalLines)
+		while(_outputText.Length < _inputText.Length)
 		{
-			//Debug.Log("HFRUWE!");
-			_text += "\n" + _s;
-
-			while(true)
+			if(_currentPosition < _inputText.Length)
 			{
-				if(_currentPosition < _text.Length)
-				{
-					GetComponent<TextMesh>().text += _text[_currentPosition++];
-					//guiText.text += _text[_currentPosition++];
-				}
-				yield return new WaitForSeconds(_delay);
+				_outputText += _inputText[_currentPosition++];
+				GetComponent<TextMesh>().text = _outputText;
 			}
+				yield return new WaitForSeconds(_delay);
 		}
+
 	}
 }
