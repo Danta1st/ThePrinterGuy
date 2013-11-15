cbb405b5a0ec54af65b53185fd795cf1881b75a2
Long Thanh Truong <ttruon08@student.aau.dk>
2013-11-15 17:07:49 +0100

    Add frequency analyzer script

--- ThePrinterGuy/Assets/Scripts/New Game Approved/ActionSequencerItem.cs
+++ ThePrinterGuy/Assets/Scripts/New Game Approved/ActionSequencerItem.cs
@@ -3,16 +3,20 @@ using System.Collections;
 
 public class ActionSequencerItem : MonoBehaviour
 {
+    #region SerializeField
+    [SerializeField]
+    private string _moduleName;
+    #endregion
+
     #region Private Variables
+    private GameObject _frequencyAnalyzerObject;
+    private FrequencyAnalyzer _frequencyAnalyzerScript;
     private GUIGameCamera _guiGameCameraScript;
     private ActionSequencerZone _actionSequencerScript;
     private string _statusZone = "";
     private int _zone = 0;
 
-    private float _speed = 40.0f;
-    private float _startTime;
-    private float _journeyLength;
-    private bool _back = false;
+    private float _multiplier = 0.0f;
     private Vector3 _startSize;
     private Vector3 _newSize;
     #endregion
@@ -28,16 +32,24 @@ public class ActionSequencerItem : MonoBehaviour
         _guiGameCameraScript = GameObject.Find("GUI List").GetComponent<GUIGameCamera>();
 
         _startSize = transform.localScale;
-        _newSize = new Vector3(transform.localScale.x*1.2f, transform.lossyScale.y*1.2f, transform.localScale.z);
+        _newSize = transform.localScale;
 
-        _startTime = Time.time;
-        _journeyLength = Vector3.Distance(_startSize, _newSize);
+        _frequencyAnalyzerObject = GameObject.Find(_moduleName).gameObject;
+
+        if(_frequencyAnalyzerObject != null)
+        {
+            _frequencyAnalyzerScript = _frequencyAnalyzerObject.GetComponent<FrequencyAnalyzer>();
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        ScaleSize();
+        if(_frequencyAnalyzerObject != null)
+        {
+            _multiplier = _frequencyAnalyzerScript.GetPitch() / 500f;
+            ScaleSize();
+        }
     }
 
     void OnTriggerEnter(Collider other)
@@ -60,31 +72,8 @@ public class ActionSequencerItem : MonoBehaviour
 
     private void ScaleSize()
     {
-        float _distCovered = (Time.time - _startTime) * _speed;
-        float _fracJourney = _distCovered / _journeyLength;
-
-        if(_fracJourney > 1.0f)
-        {
-            _startTime = Time.time;
-            _fracJourney = 0.0f;
-            if(_back)
-            {
-                _back = false;
-            }
-            else
-            {
-                _back = true;
-            }
-        }
-
-        if(_back)
-        {
-            transform.localScale = Vector3.Lerp(_newSize, _startSize, _fracJourney);
-        }
-        else
-        {
-            transform.localScale = Vector3.Lerp(_startSize, _newSize, _fracJourney);
-        }
+        _newSize = new Vector3(_startSize.x*_multiplier, _startSize.y, _startSize.z);
+        transform.localScale = _newSize;
     }
 
     public int GetZoneStatus()
---
+++ ThePrinterGuy/Assets/Scripts/New Game Approved/FrequencyAnalyzer.cs
@@ -0,0 +1,87 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FrequencyAnalyzer : MonoBehaviour {
+
+    #region Private Variables
+    //Array size
+    private int _qSamples = 1024;
+    //RMS value for 0 dB
+    private float _refValue = 0.1f;
+    //Minimum amplitude to extract pitch
+    private float _threshold = 0.02f;
+    //Sound level - Hz
+    private float _pitchValue;
+
+    //Audio samples
+    private float[] _samples;
+    //Audio spectrum
+    private float[] _spectrum;
+    private float _fSample;
+    #endregion
+
+    #region Start and Update
+	// Use this for initialization
+	void Start () {
+	    _samples = new float[_qSamples];
+        _spectrum = new float[_qSamples];
+        _fSample = AudioSettings.outputSampleRate;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        AnalyzeSound();
+	}
+    #endregion
+
+    #region Class methods
+    public float GetPitch()
+    {
+        return _pitchValue;
+    }
+
+    private void AnalyzeSound()
+    {
+        //Fill array with samples
+        audio.GetOutputData(_samples, 0);
+
+        float _sum = 0.0f;
+
+        for(int i = 0; i < _qSamples; i++)
+        {
+            // Sum squared samples
+            _sum += _samples[i]*_samples[i];
+        }
+
+        audio.GetSpectrumData(_spectrum, 0, FFTWindow.BlackmanHarris);
+        float _maxV = 0.0f;
+        int _maxN = 0;
+
+        //Find max
+        for(int i = 0; i < _qSamples; i++)
+        {
+            if(_spectrum[i] > _maxV && _spectrum[i] > _threshold)
+            {
+                _maxV = _spectrum[i];
+                //maxN is the index of max
+                _maxN = i;
+            }
+        }
+
+        //Pass the index to a float variable
+        float _fregN = _maxN;
+
+        //Interpolate index using neighbors
+        if(_maxN > 0 && _maxV < _qSamples-1)
+        {
+            float _dL =  _spectrum[_maxN-1]/_spectrum[_maxN];
+            float _dR = _spectrum[_maxN+1]/_spectrum[_maxN];
+            _fregN += 0.5f*(_dR*_dR - _dL*_dL);
+        }
+
+        //Convert index to frequency
+        _pitchValue = _fregN * (_fSample/2) / _qSamples;
+        Debug.Log(_pitchValue);
+    }
+    #endregion
+}
