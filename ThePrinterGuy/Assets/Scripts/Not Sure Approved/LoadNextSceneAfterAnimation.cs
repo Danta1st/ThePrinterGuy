b239dc4c728d6f30ab86ed0635aadcac9b4345af
Daniel Rune Jensen <danielrunejensen@gmail.com>
2013-11-19 20:28:53 +0100

    All Most Good Shiitz

--- ThePrinterGuy/Assets/Scripts/Not Sure Approved/LoadNextSceneAfterAnimation.cs
+++ ThePrinterGuy/Assets/Scripts/Not Sure Approved/LoadNextSceneAfterAnimation.cs
@@ -3,6 +3,20 @@ using System.Collections;
 
 public class LoadNextSceneAfterAnimation : MonoBehaviour {
     private GameObject Character;
+
+
+    #region Setup of Delegates
+    void OnEnable ()
+    {
+        GestureManager.OnTap += LoadNextScene;
+    }
+ 
+    void OnDisable ()
+    {
+        GestureManager.OnTap -= LoadNextScene;
+    }
+    #endregion
+
 	// Use this for initialization
 	void Start () {
         Character = gameObject;
@@ -10,12 +24,17 @@ public class LoadNextSceneAfterAnimation : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-        LoadNextScene();
+        LoadNextSceneAfterAnimationIfTrue();
 	}
 
-    public void LoadNextScene()
+    public void LoadNextSceneAfterAnimationIfTrue()
     {
         if(!gameObject.animation.isPlaying)
             Application.LoadLevel("Stage1Level1");
+    }
+
+    public void LoadNextScene(GameObject go, Vector2 screenPosition)
+    {
+        Application.LoadLevel("Stage1Level1");
     }
 }
