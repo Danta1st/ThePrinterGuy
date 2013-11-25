117195a131255a7242eb0646463e9c562c0379e4
Danny Tamsen <Danta1st@gmail.com>
2013-11-25 21:11:46 +0100

    fixed absolutely nothing, but made a simplified credits solution

---
+++ ThePrinterGuy/Assets/Scripts/New Game Approved/credits.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class credits : MonoBehaviour {
+
+	
+    public float scrollSpeed = 0.05F;
+	
+    void Update() {
+        float offset = Time.time * Mathf.Abs(scrollSpeed);
+        renderer.material.SetTextureOffset("_MainTex", new Vector2(0, offset * -1.0f));
+    }
+}
