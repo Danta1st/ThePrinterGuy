fa0169d3f888b6ed24e8bc7c9033e0a8410df40b
Danny Tamsen <Danta1st@gmail.com>
2013-11-25 23:00:51 +0100

    polished front main menu screen a bit

---
+++ ThePrinterGuy/Assets/TapToPlayMainMenu.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TapToPlayMainMenu : MonoBehaviour {
+	
+	void OnEnable()
+	{
+		GestureManager.OnTap += Disable;
+	}
+	void OnDisable()
+	{
+		GestureManager.OnTap -= Disable;
+	}
+	
+	private void Disable(GameObject go, Vector2 screenPosition)
+	{
+		if(go.name == gameObject.name)
+		{			
+			gameObject.SetActive(false);			
+		}
+	}
+}
