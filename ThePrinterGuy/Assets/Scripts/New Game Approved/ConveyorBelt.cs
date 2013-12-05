80e91bcdb293fd5a0c568859ba7e1433b1e7775c
Martin Leth Pedersen <mped10@student.aau.dk>
2013-12-05 23:22:32 +0100

    Add rolling conveyorBelt to BaseModel

---
+++ ThePrinterGuy/Assets/Scripts/New Game Approved/ConveyorBelt.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ConveyorBelt : MonoBehaviour
+{
+    private GameObject _conveyorBelt;
+
+    void Start()
+    {
+        _conveyorBelt = gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RollConvoreBelt();
+    }
+
+    public void RollConvoreBelt()
+    {
+        Vector2 thisOffset = _conveyorBelt.renderer.material.mainTextureOffset;
+        _conveyorBelt.renderer.material.mainTextureOffset = (thisOffset + new Vector2(0.0f, -0.001f));
+    }
+}
